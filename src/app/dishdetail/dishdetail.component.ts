d8c357331c797e12daed8f85a63e66c3917bd03b
Karim Tayie <karimsherif2597@gmail.com>
2019-01-21 00:07:11 +0200

    Promise Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -21,11 +21,13 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
-    this.dish = this.dishservice.getDish(id);
+    // this.dish = this.dishservice.getDish(id);
+    this.dishservice.getDish(id)
+      .then(dish => this.dish = dish);
   }
 
   goBack(): void {
     this.location.back();
   }
-  
+
 }
